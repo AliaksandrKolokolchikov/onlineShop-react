8725587633933571a21d36a4878907e28a88a0e7
alexanderpoge <flekssando@mail.ru>
2024-05-23 19:23:33 +0300

    Create components Sidebar and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,42 @@
+import footerStyles from "./../../styles/Footer.module.css";
+import headerStyle from "../../styles/Header.module.css";
+import {Link} from "react-router-dom";
+import {ROUTES} from "../../utils/Routes";
+import headerLogo from "../../images/logo.svg";
+
+
 function Footer() {
     return (
-        <div>Footer</div>
+        <section className={footerStyles.footer}>
+            <div className={headerStyle.logo}>
+                <Link to={ROUTES.HOME}>
+                    <img src={headerLogo} alt="Stuff"/>
+                </Link>
+            </div>
+            <div className={footerStyles.rights}>
+                Developed by Flekssando
+            </div>
+            <div className={footerStyles.socials}>
+                <a href="https://youtube.com" target="_blank" rel='noreferrer'>
+                    <svg className='icon'>
+                        <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`}/>
+                    </svg>
+                </a>
+
+                <a href="https://facebook.com" target="_blank" rel='noreferrer'>
+                    <svg className='icon'>
+                        <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`}/>
+                    </svg>
+                </a>
+
+                <a href="https://instagram.com" target="_blank" rel='noreferrer'>
+                    <svg className='icon'>
+                        <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`}/>
+                    </svg>
+                </a>
+            </div>
+        </section>
     )
 }
+
 export default Footer
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,25 @@
+import sidebarStyles from "../../styles/Sidebar.module.css";
+import {NavLink} from "react-router-dom";
+
+
 function Sidebar() {
     return(
-        <div>said</div>
+        <section className={sidebarStyles.sidebar}>
+            <div className={sidebarStyles.title}>CATEGORIES</div>
+            <nav>
+            <ul className={sidebarStyles.menu}>
+                <li>
+                    <NavLink to={`/categories/${1}`}>Link</NavLink>
+                </li>
+            </ul>
+            </nav>
+
+            <div className={sidebarStyles.footer}>
+                <a className={sidebarStyles.link} href='https://google.com' target='_blank'>Help</a>
+                <a className={sidebarStyles.link} href='https://google.com' target='_blank'>Terms & Conditions</a>
+            </div>
+        </section>
     )
 }
+
 export default Sidebar;

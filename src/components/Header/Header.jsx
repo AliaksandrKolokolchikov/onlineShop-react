36703d6149ca1aee6f93e283b0a2aeedb4862da8
alexanderpoge <flekssando@mail.ru>
2024-06-01 11:27:00 +0300

    create logic for search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,73 +1,119 @@
-import headerStyle from './../../styles/Header.module.css';
-import {Link, useNavigate} from "react-router-dom";
-import {ROUTES} from "../../utils/Routes";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-import headerLogo from "../../images/logo.svg";
-import headerAvatar from '../../images/avatar.jpg'
-import {useDispatch, useSelector} from "react-redux";
-import {toggleForm} from "../../features/user/userSlice";
-import {useEffect, useState} from "react";
+import styles from "../../styles/Header.module.css";
 
+import { ROUTES } from "../../utils/Routes";
 
-function Header() {
+import LOGO from "../../images/logo.svg";
+import AVATAR from "../../images/avatar.jpg";
+
+import { toggleForm } from "../../features/user/userSlice";
+import { useGetProductsQuery} from "../../features/api/apiSlice";
+
+const Header = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const { currentUser } = useSelector(({user}) => user);
-    const [values, setValues] = useState({name:'Guest', avatar:headerAvatar})
+
+    const [searchValue, setSearchValue] = useState("");
+    const { currentUser, cart } = useSelector(({ user }) => user);
+
+    const [values, setValues] = useState({ name: "Guest", avatar: AVATAR });
+
+    const { data, isLoading } = useGetProductsQuery({ title: searchValue });
+
 
     useEffect(() => {
-        if(!currentUser) return;
-        setValues(currentUser)
+        if (!currentUser) return;
+
+        setValues(currentUser);
     }, [currentUser]);
+
     const handleClick = () => {
-        if(!currentUser) dispatch(toggleForm(true))
-        else navigate(ROUTES.PROFILE)
-    }
+        if (!currentUser) dispatch(toggleForm(true));
+        else navigate(ROUTES.PROFILE);
+    };
+
+    const handleSearch = ({ target: { value } }) => {
+        setSearchValue(value);
+    };
+
     return (
-        <div className={headerStyle.header}>
-            <div className={headerStyle.logo}>
+        <div className={styles.header}>
+            <div className={styles.logo}>
                 <Link to={ROUTES.HOME}>
-                    <img src={headerLogo} alt="Stuff"/>
+                    <img src={LOGO} alt="Stuff" />
                 </Link>
             </div>
-            <div className={headerStyle.info}>
-                <div className={headerStyle.user} onClick={handleClick}>
-                    <div className={headerStyle.avatar}
-                         style={{ backgroundImage:`url(${values.avatar})` }} />
-                    <div className={headerStyle.username}>{values.name}</div>
+
+            <div className={styles.info}>
+                <div className={styles.user} onClick={handleClick}>
+                    <div
+                        className={styles.avatar}
+                        style={{ backgroundImage: `url(${values.avatar})` }}
+                    />
+                    <div className={styles.username}>{values.name}</div>
                 </div>
-                <form className={headerStyle.form}>
-                    <div className={headerStyle.icon}>
-                        <svg className='icon'>
-                             <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`} />
-                            </svg>
+
+                <form className={styles.form}>
+                    <div className={styles.icon}>
+                        <svg className="icon">
+                            <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`} />
+                        </svg>
                     </div>
-                    <div className={headerStyle.input}>
-                            <input type='#search'
-                                   name='search'
-                                   placeholder={'Search...'}
-                                   autoComplete='off'
-                                   onChange={()=>{}}
-                                   value=""/>
+                    <div className={styles.input}>
+                        <input
+                            type="search"
+                            name="search"
+                            placeholder="Search for anyting..."
+                            autoComplete="off"
+                            onChange={handleSearch}
+                            value={searchValue}
+                        />
                     </div>
-                    {/*<div className={headerStyle.box}></div>*/}
+
+                    {searchValue && (
+                        <div className={styles.box}>
+                            {isLoading ? "Loading" : !data.length ? "No results" : data.map(({ title, images, id }) => {
+                                        return (
+                                            <Link
+                                                key={id}
+                                                onClick={() => setSearchValue("")}
+                                                className={styles.item}
+                                                to={`/products/${id}`}
+                                            >
+                                                <div
+                                                    className={styles.image}
+                                                    style={{ backgroundImage: `url(${images[0]})` }}
+                                                />
+                                                <div className={styles.title}>{title}</div>
+                                            </Link>
+                                        );
+                                    })}
+                        </div>
+                    )}
                 </form>
-                <div className={headerStyle.account}>
-                    <Link to={ROUTES.HOME} className={headerStyle.favourites}   >
-                        <svg className={headerStyle['icon-fav']}>
+
+                <div className={styles.account}>
+                    <Link to={ROUTES.HOME} className={styles.favourites}>
+                        <svg className={styles["icon-fav"]}>
                             <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#heart`} />
                         </svg>
                     </Link>
-                    <Link to={ROUTES.CART} className={headerStyle.cart} >
-                        <svg className={headerStyle['icon-cart']}>
+
+                    <Link to={ROUTES.CART} className={styles.cart}>
+                        <svg className={styles["icon-cart"]}>
                             <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#bag`} />
                         </svg>
-                        <span className={headerStyle.count}>2</span>
+                        {!!cart.length && (
+                            <span className={styles.count}>{cart.length}</span>
+                        )}
                     </Link>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default Header;
